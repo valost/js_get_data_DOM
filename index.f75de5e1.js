e2826903a9b1641a6bb385f8a71615668a0662b9
valost <valerieostrovska@yahoo.com>
2024-07-11 17:41:51 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,19 @@
+"use strict";
+const populationSpanArr = [
+    ...document.querySelectorAll(".population")
+];
+const populationNumArr = [];
+let populationTotal = 0;
+for(let i = 0; i < populationSpanArr.length; i++){
+    const spanText = populationSpanArr[i].textContent;
+    const populationNum = parseInt(spanText.replace(/,/g, ""), 10);
+    populationNumArr.push(populationNum);
+    populationTotal += populationNum;
+}
+const populationAverage = populationTotal / populationNumArr.length;
+const totalPopulationSpan = document.querySelector(".total-population");
+const averagePopulationSpan = document.querySelector(".average-population");
+totalPopulationSpan.textContent = populationTotal.toLocaleString("en-US");
+averagePopulationSpan.textContent = populationAverage.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
